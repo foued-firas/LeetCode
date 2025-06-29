9d0531895fd491872d5761671a408370644dd6be
Firas Foued <108490487+foued-firas@users.noreply.github.com>
2025-06-29 21:53:08 +0100

    Time: 16 ms (60.2%), Space: 12.5 MB (12.65%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        if len(word1) >= 1 and len(word2) >= 1:
+            word1 = word1.lower()
+            word2 = word2.lower()
+            merged = []
+            
+            if len(word1) == len(word2):
+                k = len(word2)
+                for i in range(k):
+                    merged.append(word1[i])
+                    merged.append(word2[i])
+            
+            elif len(word1) > len(word2):
+                k = len(word2)
+                for i in range(k):
+                    merged.append(word1[i])
+                    merged.append(word2[i])
+                merged.extend(word1[k:])   # fix here: extend the leftover substring
+            
+            else:
+                k = len(word1)
+                for i in range(k):
+                    merged.append(word1[i])
+                    merged.append(word2[i])
+                merged.extend(word2[k:])   # fix here: extend the leftover substring
+            
+            resultat = ''.join(merged)
+            return resultat
+        else:
+            return ""
